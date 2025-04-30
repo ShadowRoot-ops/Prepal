6ef3f70b7e4f1be22c0485c9e11a6e319baa59ca
ShadowRoot-ops <82092440+ShadowRoot-ops@users.noreply.github.com>
2025-04-30 12:10:27 +0530

    eslint config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
-import { BackgroundLines } from "@/components/ui/background-lines";
 import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
